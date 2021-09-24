ea9fc9ae613934587c25bb26b5abaf45b35e0033
Jack hou <jackhou921@qq.com>
2021-09-24 20:37:54 +0800

    yolov5

--- test.py
+++ test.py
@@ -306,9 +306,9 @@ def run(data,
 
 def parse_opt():
     parser = argparse.ArgumentParser(prog='test.py')
-    parser.add_argument('--data', type=str, default='Apple_data/apple.yaml', help='dataset.yaml path')
+    parser.add_argument('--data', type=str, default='firedata666/fire666.yaml', help='dataset.yaml path')
     # parser.add_argument('--data', type=str, default='yolov5fire/VOCdevkit/fire.yaml', help='dataset.yaml path')
-    parser.add_argument('--weights', nargs='+', type=str, default='runs/train/exp45/weights/best.pt', help='model.pt path(s)')
+    parser.add_argument('--weights', nargs='+', type=str, default='runs/train/exp71/weights/last.pt', help='model.pt path(s)')
     parser.add_argument('--batch-size', type=int, default=16, help='batch size')
     parser.add_argument('--imgsz', '--img', '--img-size', type=int, default=640, help='inference size (pixels)')
     parser.add_argument('--conf-thres', type=float, default=0.001, help='confidence threshold')
